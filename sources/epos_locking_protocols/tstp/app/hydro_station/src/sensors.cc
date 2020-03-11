f214faeecdbe42037d54ebbca589460f5d23aff4
Sevothart <lukasmatheus177@gmail.com>
2020-03-11 08:40:52 -0300

    Clean up

--- sources/epos_locking_protocols/tstp/app/hydro_station/src/sensors.cc
+++
@@ -1,6 +0,0 @@
-#include "../include/sensors.h"
-
-// this is needed because we treat the interrupt in a static function
-Pluviometric_Sensor * Pluviometric_Sensor::_instance = 0;
-
-bool Pluviometric_Sensor::_debouncing = false;
